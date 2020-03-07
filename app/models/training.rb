0b1f50870ab3f4758f0b86975f7f2d06582c27cc
Guitguitou <guillaume.imary@gmail.com>
2020-03-07 15:09:31 +0100

    trainingscontroller modified + training.rb validation

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,12 +1,11 @@
 class Training < ApplicationRecord
   SEX = ["Masculin", "Féminin", "Mixte"]
-  GROUPS = ["Elite", "Groupe 1", "Groupe 2", "Groupe 3", "Groupe 4", "Groupe 5", "Coupe de France", "M20", "M17", "M15", "M13", "Baby-volley"]
   has_many :bookings, dependent: :destroy
   belongs_to :group
   has_one_attached :photo
   validates :name, presence: true
+  validates :training_type, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
   validates :sex, presence: true, inclusion: { in: SEX }
-
 end
